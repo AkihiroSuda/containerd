9755ce2c961bc4ce52cd180954788aae6a3096a5
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-06-15 08:50:20 +0000

    [WIP] client: add Export() for exporting image in OCI format
    Usage:
    
        $ dist export /tmp/oci-busybox docker.io/library/busybox:latest
    
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

---
+++ cmd/dist/export.go
@@ -0,0 +1,94 @@
+package main
+
+import (
+	"github.com/containerd/containerd"
+	"github.com/containerd/containerd/reference"
+	digest "github.com/opencontainers/go-digest"
+	ocispec "github.com/opencontainers/image-spec/specs-go/v1"
+	"github.com/pkg/errors"
+	"github.com/urfave/cli"
+)
+
+var exportCommand = cli.Command{
+	Name:      "export",
+	Usage:     "export an image",
+	ArgsUsage: "[flags] <directory> <local>",
+	Description: `Export an image
+`,
+	Flags: []cli.Flag{
+		cli.StringFlag{
+			Name:  "format",
+			Value: "oci",
+			Usage: "Format",
+		},
+		cli.StringFlag{
+			Name:  "oci-ref-name",
+			Value: "",
+			Usage: "Override org.opencontainers.image.ref.name annotation",
+		},
+		cli.StringFlag{
+			Name:  "manifest",
+			Usage: "Digest of manifest",
+		},
+		cli.StringFlag{
+			Name:  "manifest-type",
+			Usage: "Media type of manifest digest",
+			Value: ocispec.MediaTypeImageManifest,
+		},
+	},
+	Action: func(clicontext *cli.Context) error {
+		var (
+			dir   = clicontext.Args().First()
+			local = clicontext.Args().Get(1)
+			desc  ocispec.Descriptor
+		)
+
+		ctx, cancel := appContext(clicontext)
+		defer cancel()
+
+		client, err := getClient(clicontext)
+		if err != nil {
+			return err
+		}
+
+		if manifest := clicontext.String("manifest"); manifest != "" {
+			desc.Digest, err = digest.Parse(manifest)
+			if err != nil {
+				return errors.Wrap(err, "invalid manifest digest")
+			}
+			desc.MediaType = clicontext.String("manifest-type")
+		} else {
+			img, err := client.ImageService().Get(ctx, local)
+			if err != nil {
+				return errors.Wrap(err, "unable to resolve image to manifest")
+			}
+			desc = img.Target
+		}
+
+		if desc.Annotations == nil {
+			desc.Annotations = make(map[string]string)
+		}
+		if ociRefName := determineOCIRefName(local); ociRefName != "" {
+			desc.Annotations[ocispec.AnnotationRefName] = ociRefName
+		}
+		if ociRefName := clicontext.String("oci-ref-name"); ociRefName != "" {
+			desc.Annotations[ocispec.AnnotationRefName] = ociRefName
+		}
+		if format := clicontext.String("format"); format != "oci" {
+			return errors.Errorf("unsupported format: %s", format)
+		}
+		return client.Export(ctx,
+			desc,
+			containerd.WithOCIDirectoryExportation(dir),
+		)
+	},
+}
+
+func determineOCIRefName(local string) string {
+	refspec, err := reference.Parse(local)
+	if err != nil {
+		return ""
+	}
+	tag, _ := reference.SplitObject(refspec.Object)
+	return tag
+}
