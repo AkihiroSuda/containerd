d1e273293a1bd0dfa20f91d8dee2bd497fa66b67
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-08-17 03:39:12 +0000

    mount: fix read-only bind (#1368)
    Port over
    https://github.com/moby/moby/blob/3a1ab5b479ce843648cf676fbaaf2bec9e040dce/pkg/mount/mounter_linux.go
    
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

--- mount/mount_linux.go
+++ mount/mount_linux.go
@@ -6,9 +6,47 @@ import (
 	"golang.org/x/sys/unix"
 )
 
+const (
+	// ptypes is the set propagation types.
+	ptypes = unix.MS_SHARED | unix.MS_PRIVATE | unix.MS_SLAVE | unix.MS_UNBINDABLE
+
+	// pflags is the full set valid flags for a change propagation call.
+	pflags = ptypes | unix.MS_REC | unix.MS_SILENT
+
+	// broflags is the combination of bind and read only
+	broflags = unix.MS_BIND | unix.MS_RDONLY
+)
+
+// isremount returns true if either device name or flags identify a remount request, false otherwise.
+func isremount(device string, flags int) bool {
+	// We treat device "" and "none" as a remount request to provide compatibility with
+	// requests that don't explicitly set MS_REMOUNT such as those manipulating bind mounts.
+	return flags&unix.MS_REMOUNT != 0 || device == "" || device == "none"
+}
+
 func (m *Mount) Mount(target string) error {
 	flags, data := parseMountOptions(m.Options)
-	return unix.Mount(m.Source, target, m.Type, uintptr(flags), data)
+	oflags := flags &^ ptypes
+	if !isremount(m.Source, flags) || data != "" {
+		// Initial call applying all non-propagation flags for mount
+		// or remount with changed data
+		if err := unix.Mount(m.Source, target, m.Type, uintptr(oflags), data); err != nil {
+			return err
+		}
+	}
+
+	if flags&ptypes != 0 {
+		// Change the propagation type.
+		if err := unix.Mount("", target, "", uintptr(flags&pflags), ""); err != nil {
+			return err
+		}
+	}
+
+	if flags&broflags == broflags {
+		// Remount the bind to apply read only.
+		return unix.Mount("", target, "", uintptr(flags|unix.MS_REMOUNT), "")
+	}
+	return nil
 }
 
 func Unmount(mount string, flags int) error {
