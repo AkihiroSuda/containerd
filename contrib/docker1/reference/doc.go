0002f404f29a668f3de1ac9e8e8de39ebf4db48e
Akihiro Suda <suda.akihiro@lab.ntt.co.jp>
2017-10-05 08:52:10 +0000

    contrib: Docker v1 image importer
    For usage, please refer to `ctr images import --help`.
    
    Signed-off-by: Akihiro Suda <suda.akihiro@lab.ntt.co.jp>

---
+++ contrib/docker1/reference/doc.go
@@ -0,0 +1,4 @@
+// Package reference was imported from docker/distribution@e5b5e44386f7964a1f010a2b8b7687d073215bbb
+// so as not to introduce extra vendoring.
+// This package will soon go away.
+package reference
